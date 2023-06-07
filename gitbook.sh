78ac6c8d5ac7003d43128d4bb473660fae6b41f3
pompeychen <1301699286@qq.com>
2023-06-07 10:57:49 +0800

    修复bug

--- gitbook.sh
+++ gitbook.sh
@@ -2,7 +2,7 @@
 #bash 脚本 调用 docker gitbook
 #cp gitbook.sh ~/.local/bin
 #编辑~/.bashrc 添加alias gitbook='gitbook.sh'
-function get_port
+function get_port()
 {
     first_port=1000
     last_port=65535
@@ -10,18 +10,18 @@ function get_port
     do
         msg=`netstat -nlat|tail -n +3|awk '{print $4}'|grep $port`
         if [ "$msg"x == "x" ]; then
-            break
+            echo $port
+            exit 0
         fi
     done
 
     if [ "$port" == 65536 ]; then
         exit 1
     fi
-    echo $port
 }
 
 if [ "$1" == serve ]; then
-    port= `get_port`
+    port=`get_port`
     docker run -d --rm -v `pwd`:/gitbook -p $port:$port gitbook --port $port serve
 else
     docker run --rm -v `pwd`:/gitbook gitbook $1
